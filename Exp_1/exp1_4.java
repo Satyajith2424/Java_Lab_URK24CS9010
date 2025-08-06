37fbc02b95db14618539b259b9a2487d2ad1eff5
Dandabathina Satyajith <dandabathinasatyajith@karunya.edu.in>
2025-08-06 22:29:11 +0530

    Update exp1_4.java

--- Exp_1/exp1_4.java
+++ Exp_1/exp1_4.java
@@ -1,31 +1,72 @@
 import java.util.Scanner;
+
 import java.util.Random;
+
 public class exp1_4 {
-	
+
+
+
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
 
-        int Random_num = rand.nextInt(100) + 1; // 1 to 100
-        int User_guess;
+		Random rand = new Random();
 
-        System.out.println("Guess a number between 1 and 100:");
+		int random_num = rand.nextInt(100)+1;
 
-        while (true) {
-            System.out.print("Enter your guess: ");
-            User_guess = sc.nextInt();
+		int i =1;
 
-            if (User_guess > Random_num) {
-                System.out.println("Too High");
-            } else if (User_guess < Random_num) {
-                System.out.println("Too Low");
-            } else {
-                System.out.println("Correct Guess!");
-                break; 
-            }
-        }
+		while(i<=7) {
 
+			if(i>6) {
+
+				System.out.println("Game Over! Max Attempts Reached");
+
+				break;
+
+			}
+
+			System.out.println("Attempt No:"+i);
+
+			System.out.println("Enter the number You Guessed");
+
+			int user_guess =sc.nextInt();
+
+			
+
+			if(user_guess>random_num) {
+
+				System.out.println("Too High");
+
+			}
+
+			else if(user_guess<random_num) {
+
+				System.out.println("Too Low");
+
+			}
+
+			else {
+
+				System.out.println("Correct Guess");
+
+				break;
+
+			}
+
+			i++;
+
+			
+
+		
+
+		}
 
 	}
 
+
+
 }
+
+}
+
